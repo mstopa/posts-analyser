51039cc2b9cc41140fc7777b30803446d6852e7c
mstopa <m.stopa94@gmail.com>
2022-10-30 18:09:14 +0100

    Download HF models in project build

--- ml/setup.py
+++ ml/setup.py
@@ -1,19 +1,122 @@
 from pathlib import Path
+
+from setuptools import Command
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from setuptools.command.egg_info import egg_info
 from setuptools import setup
 
 from version import __version__ as package_version
 
 project_root = Path(__file__).parent
 
+
+class BuildWithModelData(Command):
+    """Command to fetch model data in the building stage"""
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Fetch models data"""
+        import requests
+        from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+        model_name = model_name = f"cardiffnlp/twitter-roberta-base-sentiment"
+        labels_source = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+        
+        model_dir = project_root / "model"
+
+        tokenizer_file = model_dir / "tokenizer"
+        model_file = model_dir /  "model"
+        labels_file = model_dir / "labels.txt"
+
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenizer.save_pretrained(tokenizer_file)
+
+        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        model.save_pretrained(model_file)
+
+        label_response = requests.get(labels_source)
+        with open(labels_file, 'wb') as f:
+            f.write(label_response.content)
+        
+        with open("src/model_dir.py", "w") as f:
+            f.write(f'model_dir = "{model_dir}"\n')
+
+
+class Build(build_py):
+    """Command for Python modules build."""
+
+    def __init__(self, dist):
+        """Create a sub-command to execute."""
+        self.subcommand = BuildWithModelData(dist)
+        super().__init__(dist)
+
+    def run(self):
+        """Build Python and GRPC modules."""
+        super().run()
+        self.subcommand.run()
+
+
+class Develop(develop):
+    """Command for develop installation."""
+
+    def __init__(self, dist):
+        """Create a sub-command to execute."""
+        self.subcommand = BuildWithModelData(dist)
+        super().__init__(dist)
+
+    def run(self):
+        """Build GRPC modules before the default installation."""
+        self.subcommand.run()
+        super().run()
+
+
+class CustomEggInfo(egg_info):
+    """Command for pip installation."""
+
+    def __init__(self, dist):
+        """Create a sub-command to execute."""
+        self.subcommand = BuildWithModelData(dist)
+        super().__init__(dist)
+
+    def run(self):
+        """Build GRPC modules before the default installation."""
+        self.subcommand.run()
+        super().run()
+
+
+class CustomInstall(install):
+    """Command for pip installation."""
+
+    def __init__(self, dist):
+        """Create a sub-command to execute."""
+        self.subcommand = BuildWithModelData(dist)
+        super().__init__(dist)
+
+    def run(self):
+        """Build GRPC modules before the default installation."""
+        self.subcommand.run()
+        super().run()
+
+
 setup(
     name="posts_sentiment_analyser",
     version=package_version,
-    description="Machine learning model for sentiment analysis",
+    description="ML model server for sentiment analysis",
     author="Maciek Stopa",
-    author_email="mstopa@student.agh.edu.pl",
+    author_email="m.stopa94@gmail.com",
     url="https://github.com/cansubmarinesswim/posts-analyser",
     packages=['posts_sentiment_analyser'],
     package_dir={'posts_sentiment_analyser':'src'},
+    include_package_data=True,
     install_requires=[
         "flask",
         "py-healthcheck",
@@ -23,6 +126,14 @@ setup(
         "transformers",
         "waitress"
     ],
+    setup_requires=["requests", "transformers"],
     long_description=(project_root / "README.md").read_text(),
     long_description_content_type="text/markdown",
+    cmdclass={
+        "build_py": Build,
+        "build_grpc": BuildWithModelData,
+        "develop": Develop,
+        "egg_info": CustomEggInfo,
+        "install": CustomInstall,
+    }
 )
--- ml/src/app.py
+++ ml/src/app.py
@@ -1,5 +1,5 @@
-import logging
-from typing import Dict
+from pathlib import Path
+from typing import List, Tuple
 
 from flask import Flask, jsonify, request
 from healthcheck import HealthCheck
@@ -8,20 +8,21 @@ from scipy.special import softmax
 
 import posts_sentiment_analyser.model_utils as model_utils
 
-logger = logging.getLogger("ml_model")
+try:
+    from posts_sentiment_analyser.model_dir import model_dir
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("Run setup.py first to fetch model components.")
 
-# Tasks:
-# emoji, emotion, hate, irony, offensive, sentiment
-# stance/abortion, stance/atheism, stance/climate, stance/feminist, stance/hillary
-task = "sentiment"
-model_name = f"cardiffnlp/twitter-roberta-base-{task}"
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
+model_dir = Path(model_dir)
+for component in ("tokenizer", "model", "labels.txt"):
+    if not (model_dir / component).exists():
+        raise FileNotFoundError(f"{model_dir / component} not found. Run setup.py first to download model components.")
 
-tokenizer = model_utils.load_tokenizer(model_name)
-model = model_utils.load_model(model_name)
-labels = model_utils.load_labels(mapping_link)
+tokenizer = model_utils.load_tokenizer(f"{model_dir / 'tokenizer'}")
+model = model_utils.load_model(f"{model_dir / 'model'}")
+labels = model_utils.load_labels(f"{model_dir / 'labels.txt'}")
 
-def _classify(text: str) -> Dict:
+def _classify(text: str) -> List[Tuple[str, str]]:
     encoded_input = tokenizer(text, return_tensors='pt')
     output = model(**encoded_input)
     scores = output[0][0].detach().numpy()
