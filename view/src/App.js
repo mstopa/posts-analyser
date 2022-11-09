8fbc20973831858e7c13caf3ee5274816232f9bd
mstopa <m.stopa94@gmail.com>
2022-11-09 18:39:51 +0100

    Initial view version

--- view/src/App.js
+++ view/src/App.js
@@ -1,7 +1,54 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [post, setPost] = useState({
+    text: "",
+  });
+  const [postsList, setPostsList] = useState();
+  const [error, setError] = useState();
+
+  const handleSubmit = (event) => {
+    // Handle
+  };
+
+  const handleInputChange = (event) => {
+    setPost({
+      ...post,
+      text: event.target.value,
+    });
+  };
+
+  const handleDelete = (id) => {
+    // Handle
+  };
+
+  return (
+    <div className="App">
+      <h1>Add a new post</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="textarea"
+          value={post.text}
+          placeholder="Your post..."
+          onChange={handleInputChange}
+        ></input>
+        <button type="submit">Add post</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+    <ol>
+    {postsList?.map((postItem) => (
+      <li
+      onClick={() => {
+        handleDelete(postItem.id);
+      }}>
+        {postItem.text}
+      </li>
+    ))}
+    </ol>
+    </div>
+  );
 }
 
 export default App;
